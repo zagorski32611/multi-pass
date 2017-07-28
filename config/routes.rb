d560d1598d53ac18c6e268b33dec74b7a1fa3e73
Joe Zagorski <j.j.zagorski@gmail.com>
2017-07-28 12:48:43 -0400

    added remaining forms and routes for username, password, and user-added credentials

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   get '/financial' => 'financial#index'
   get '/work' => 'work#index'
   get '/other' => 'other#index'
+
+  resources :multipass_username
+  resources :multipass_password
+  resources :website
+  resources :username
+  resources :password
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
